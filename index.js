8b67983ab384f4b9dc6989a2db9f71c4c33a0d80
cesar <cesartavaresfilho9@gmail.com>
2025-10-01 03:41:59 -0300

    feat(p1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ function coordenadas(){
                 console.log("Cidade: " + cidade.local_names?.pt)
                 console.log("Latitude: " + cidade.lat)
                 console.log("Longitude: " + cidade.lon)
+                condicoes(cidade.local_names.pt, cidade.lat, cidade.lon)
             }
         });
     }).catch((err) => {
         console.log(err)
     })
+}
+
+async function condicoes(cidade, LAT, LON) {
+    let url = `${PROTOCOLO_BASICO}://${URL_CURRENT}?lat=${LAT}&lon=${LON}&appid=${APPID_WEATHER}&units=metric&lang=pt`
+    try {
+        const resposta = await axios.get(url)
+
+        console.log("\nCidade: "+cidade)
+        console.log("Sensação térmica: " + resposta.data.main.feels_like)
+        console.log("Descrição: " + resposta.data.weather[0].description)
+    } catch(err) {
+        console.log(err)
+    }
 }
 
 coordenadas()
