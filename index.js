a2038756da9cdd889c3695254397efd4cbd98a82
cesar <cesartavaresfilho9@gmail.com>
2025-10-01 02:39:30 -0300

    feat(p1): obtem coords a partir de cidade

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 require('dotenv').config()
 const axios =require('axios')
+const {PROTOCOLO_BASICO, URL_GEO, URL_CURRENT, APPID_WEATHER, Q, LIMIT} = process.env
+
+function coordenadas(){
+    let url =`${PROTOCOLO_BASICO}://${URL_GEO}?q=${Q}&limit=${LIMIT}&appid=${APPID_WEATHER}`
+    axios.get(url).then((resposta) => {
+        console.log(resposta.data)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
