4867895c4e8bb64190c980074091baa7d1341d52
cesar <cesartavaresfilho9@gmail.com>
2025-10-01 03:08:20 -0300

    feat(p1): obtem coords a partir de cidade

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const {PROTOCOLO_BASICO, URL_GEO, URL_CURRENT, APPID_WEATHER, Q, LIMIT} = proces
 
 function coordenadas(){
     let url =`${PROTOCOLO_BASICO}://${URL_GEO}?q=${Q}&limit=${LIMIT}&appid=${APPID_WEATHER}`
+    let i=0;
     axios.get(url).then((resposta) => {
-        console.log(resposta.data)
+        resposta.data.forEach((cidade, i) => {
+            if(!cidade.local_names || cidade.local_names.pt ===undefined){
+                return;
+            }
+            else{
+                console.log("================================")
+                console.log("Cidade: " + cidade.local_names?.pt)
+                console.log("Latitude: " + cidade.lat)
+                console.log("Longitude: " + cidade.lon)
+            }
+        });
     }).catch((err) => {
         console.log(err)
     })
 }
+
+coordenadas()
